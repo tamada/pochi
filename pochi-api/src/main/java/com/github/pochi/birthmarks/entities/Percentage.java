e16f363054009a98f4f716e449b4917c09447cf6
Haruaki Tamada <tamada@users.noreply.github.com>
2018-09-27 07:02:11 +0900

    add monitoring api interface for extracting/comparing birthmarks

---
+++ pochi-api/src/main/java/com/github/pochi/birthmarks/entities/Percentage.java
@@ -0,0 +1,20 @@
+package com.github.pochi.birthmarks.entities;
+
+public class Percentage {
+    private long numerator;
+    private long denominator;
+
+    public Percentage(long numerator, long denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    @Override
+    public String toString() {
+        return toString("%3.2g%%");
+    }
+
+    public String toString(String formatter) {
+        return String.format(formatter, 100.0 * numerator /  denominator);
+    }
+}
