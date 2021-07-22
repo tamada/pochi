9ee2b12cf0803f937c256111122f9ce5249986d9
Haruaki Tamada <tamada@users.noreply.github.com>
2021-07-22 09:53:19 +0900

    update build docker script adding double quotes

--- bin/build_dockers.sh
+++ bin/build_dockers.sh
@@ -5,7 +5,7 @@ VERSION="2.3.17"
 echo "build docker image for pochi-${VERSION}"
 
 for i in dockers/*; do
-  name=$(basename $i)
-  docker build --force-rm=true --rm=true --no-cache=true --tag=ghcr.io/tamada/$name:$VERSION $i
-  docker tag ghcr.io/tamada/$name:$VERSION ghcr.io/tamada/$name:latest
+  name="$(basename $i)"
+  docker build --force-rm=true --rm=true --no-cache=true --tag=ghcr.io/tamada/"$name":$VERSION $i
+  docker tag ghcr.io/tamada/"$name":$VERSION ghcr.io/tamada/"$name":latest
 done
